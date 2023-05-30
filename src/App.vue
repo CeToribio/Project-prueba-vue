425ce178e2fc8c4a1b70d1d45463bdfef1d77ab4
CeToribio <anctoribio@gmail.com>
2023-05-29 19:35:32 -0500

    conceptos esenciales en vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {ref, computed} from "vue";
+
+const counter = ref(0);
+
+const add = ( ) => counter.value++ 
+
+const rest = ( ) => counter.value--
+
+const reset =() => (counter.value = 0)
+
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Counter</h1>
+  <p>{{ counter }}</p>
+  <button v-on:click="add">Add</button>
+  <button @:click="rest">Rest</button>
+  <button v-on:click="reset">Reset</button>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+h1 {
+  color: green;
 }
 </style>
